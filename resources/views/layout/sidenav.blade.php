fdc9d474ee8fc8789f26d1739a8542e87a1aa2d6
bobbysevri <bobbysevri@gmail.com>
2020-10-14 19:42:25 +0700

    tes branc

--- resources/views/layout/sidenav.blade.php
+++ resources/views/layout/sidenav.blade.php
@@ -3,7 +3,7 @@
         <ul class="nav navbar-nav flex-row">
             <li class="nav-item mr-auto"><a class="navbar-brand" href="#">
                     <div class="brand-logo"></div>
-                    <h2 class="brand-text mb-0">SIMS</h2>
+                    <h2 class="brand-text mb-0">SIMS 0.2</h2>
                 </a></li>
             <li class="nav-item nav-toggle"><a class="nav-link modern-nav-toggle pr-0" data-toggle="collapse"><i
                         class="icon-x d-block d-xl-none font-medium-4 primary toggle-icon feather icon-disc"></i><i
